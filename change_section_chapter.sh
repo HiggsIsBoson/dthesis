7a454e25518bac6eac3e6dc2d70f749b1874d98e
shion <shion@cern.ch>
2025-08-10 16:48:50 +0900

    Clean up

--- change_section_chapter.sh
+++
@@ -1,6 +0,0 @@
-for file in `ls tex/*/*.tex`; do 
-#    sed -i -e "s#\\subsubsection#\\subsection#g; s#\\subsection#\\section#g; s#\\section{#\\chapter{#g"  ${file}
-    sed -i -e "s#\\chapter#\\section#g"  ${file}
-#    break
-done
-
